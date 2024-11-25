88f2fc3992bfbb8ef56ad1f0081c508941ed2595
Ethan Lanting <ethanlanting@gmail.com>
2024-11-25 16:53:26 -0600

    Refactor layout and Navbar styles for improved responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ import CoffeeButton from "./buttons/CoffeeButton";
 
 const Navbar = () => {
   return (
-    <nav className="absolute left-1/2 top-0 z-50 flex w-full -translate-x-1/2 items-center justify-center px-6">
+    <nav className="absolute left-1/2 top-0 z-50 flex w-full -translate-x-1/2 items-center justify-center px-3 sm:px-6">
       <div className="mt-4 flex w-full max-w-[54rem] items-center justify-between gap-x-4 rounded-3xl border bg-background px-7 py-5">
         <div className="flex grow basis-0 items-center">
           <Link
